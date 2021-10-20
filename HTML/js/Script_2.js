16c97c92e9671923567eb1f4d73b8da17d6abeac
giorgiasampo <giorgia.sampo@studio.unibo.it>
2021-10-20 13:20:26 +0200

    changes

--- HTML/js/Script_2.js
+++ HTML/js/Script_2.js
@@ -7,27 +7,66 @@ function issue_selected(issue_id, name_article){
 function from_issue(){
   var issue_id = localStorage.issue_id;
   var name_article = localStorage.name_article;
+  readIssues(issue_id, name_article);
   var articles = JSON.parse(localStorage.articles);
   var sources = JSON.parse(localStorage.sources);
   console.log(issue_id, name_article, articles, sources);
   localStorage.clear();
-  readIssues(issue_id, name_article);
   if (issue_id == "Deep Thoughts" || issue_id == "Magratears" || issue_id == "Vogueon" && name_article == ""){
     document.getElementById("article_1").click();
     document.getElementById("article_2").click();
     document.getElementById("article_3").click();
+    let promises = [];
     for (i=0; i<3; i++){
-      var name =  "/data/" + issue_id + "/" + articles[i] + ".txt";
-        var id = "text_3_" + (i+1);
-        alert (name);
-        alert (id);
-        fetch(name)
-       .then(response => response.text())
-          .then(text => document.getElementById(id).innerHTML=(text))
-                return;
+  		var name =  "/data/" + issue_id + "/" + articles[i] + ".txt";
+      var id = "text_3_" + (i+1);
+      promises.push(fetch(name));
+    }
+    Promise.all(promises)
+    .then (function handleData(data){
+      return fetch(name)
+      .then (response => {
+        if (response.ok) return response.json();
+        throw new Error(response.statusText);
+      });
+  })
+  .catch(function handleError(error) {
+  console.log("Error" + error);
+  });
     }
   }
-  }
+
+  function readIssues(issue_id, name_article){
+    readJSon("js/issues.json", function(content){
+      var issues = JSON.parse(content);
+      var len = issues.length;
+      var articles =[];
+      var sources =[];
+      for (i=0; i<len; i++){
+        if (issues[i].name == issue_id){
+        articles = issues[i].articles;
+        sources = issues[i].sources;
+        localStorage.setItem("articles", JSON.stringify(articles));
+        localStorage.setItem("sources", JSON.stringify(sources));
+        }
+        }
+      })
+      }
+
+      function readJSon(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+    
+    
+      
 
 function getCheckedBoxes(chkboxClass) {
   var checkboxes = document.getElementsByName(chkboxClass);
@@ -59,32 +98,3 @@ function getCheckedBoxes(chkboxClass) {
       document.getElementById('three_article').classList.remove('display');
     }
 }
-
-function readJSon(file, name, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-function readIssues(issue_id, name_article){
-  readJSon("js/issues.json", name, function(content){
-    var issues = JSON.parse(content);
-    var len = issues.length;
-    var articles =[];
-    var sources =[];
-    for (i=0; i<len; i++){
-      if (issues[i].name == issue_id){
-      articles = issues[i].articles;
-      sources = issues[i].sources;
-      localStorage.setItem("articles", JSON.stringify(articles));
-      localStorage.setItem("sources", JSON.stringify(sources));
-      }
-      }
-    })
-    }
