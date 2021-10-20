f1168e9f92703bb5de0f75a8d9cc121817e7d6d3
giorgiasampo <giorgia.sampo@studio.unibo.it>
2021-10-20 15:26:35 +0200

    Javascript almost done + definitive json

--- HTML/js/Script_2.js
+++ HTML/js/Script_2.js
@@ -1,39 +1,36 @@
 function issue_selected(issue_id, name_article){
+  localStorage.clear();
   var newWin = window.open("articles_viewer.html", "_self");
-      localStorage.setItem("issue_id", issue_id);
-      localStorage.setItem("name_article",name_article);
+      readIssues(issue_id, name_article);
   }
 
 function from_issue(){
   var issue_id = localStorage.issue_id;
   var name_article = localStorage.name_article;
-  readIssues(issue_id, name_article);
-  var articles = JSON.parse(localStorage.articles);
-  var sources = JSON.parse(localStorage.sources);
-  console.log(issue_id, name_article, articles, sources);
-  localStorage.clear();
-  if (issue_id == "Deep Thoughts" || issue_id == "Magratears" || issue_id == "Vogueon" && name_article == ""){
+  if (issue_id == "Deep-Thoughts" || issue_id == "Magratears" || issue_id == "Vogueon" && name_article == ""){
     document.getElementById("article_1").click();
     document.getElementById("article_2").click();
     document.getElementById("article_3").click();
-    let promises = [];
-    for (i=0; i<3; i++){
-  		var name =  "/data/" + issue_id + "/" + articles[i] + ".txt";
-      var id = "text_3_" + (i+1);
-      promises.push(fetch(name));
+    article_selector(issue_id, name_article, 3);
+  }
+  }
+
+  function article_selector(issue, article, num_article){
+    var articles = JSON.parse(localStorage.articles);
+    var sources = JSON.parse(localStorage.sources);
+    console.log(issue, article, articles, sources);
+    for (i=0; i<num_article; i++){
+  		var name =  "/data/" + issue + "/" + articles[i] + ".txt";
+      var id = "text_" + num_article + "_" + (i+1);
+      populator (name, id);
     }
-    Promise.all(promises)
-    .then (function handleData(data){
-      return fetch(name)
-      .then (response => {
-        if (response.ok) return response.json();
-        throw new Error(response.statusText);
-      });
-  })
-  .catch(function handleError(error) {
-  console.log("Error" + error);
-  });
-    }
+  }
+
+  function populator(article, position){
+    fetch(article)
+ 			 .then(response => response.text())
+  				.then(text => document.getElementById(position).innerHTML=(text))
+                return;
   }
 
   function readIssues(issue_id, name_article){
@@ -46,6 +43,8 @@ function from_issue(){
         if (issues[i].name == issue_id){
         articles = issues[i].articles;
         sources = issues[i].sources;
+        localStorage.setItem("issue_id", issue_id);
+        localStorage.setItem("name_article", name_article);
         localStorage.setItem("articles", JSON.stringify(articles));
         localStorage.setItem("sources", JSON.stringify(sources));
         }
