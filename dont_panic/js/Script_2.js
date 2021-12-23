09abdec86f3f09ad18153145d6f702ed981ab0d9
GiuliaVenditti <giulia.venditti@studio.unibo.it>
2021-12-23 11:55:34 +0100

    l

--- dont_panic/js/Script_2.js
+++ dont_panic/js/Script_2.js
@@ -336,7 +336,7 @@ function styleSwitch(id) {
   var theme = document.getElementsByTagName("link")[1];
   var new_style = "dont_panic/" + id + ".css";
 
-  console.loog(theme);
+  console.log(theme);
 
 
   if (id != "no_style") {
